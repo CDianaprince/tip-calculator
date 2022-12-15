eb7197b9f31754046647469f2a8debb2407465e3
unknown <dcarol.prince@gmail.com>
2022-12-15 12:51:11 -0600

    comenzando trabajo de funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import suma from './modules/tipCalculator.js';
+// Constante de lps elementos de HTML
+const main = document.getElementById('main');
+const form = document.getElementById('main-form')
 
-const resultado = suma (4, 9)
-
-console.log(resultado);
+//Crear un evento de submit para la forma
+form.addEventListener('submit', (e) => {
+//Evitar la action por defecto
+e.preventDefault();
+});
