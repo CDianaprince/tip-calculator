f3d4534ab306680a46aa139f949b62009918bf78
unknown <dcarol.prince@gmail.com>
2022-12-15 18:37:53 -0600

    extraccion de informacion del formulario y realizamos calculos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,52 @@
-import suma from './modules/tipCalculator.js';
+import tipCalculator from './modules/tipCalculator.js';
+
+/*import totaFinal from './modules/TotalFinal.js';*/
+
 // Constante de lps elementos de HTML
 const main = document.getElementById('main');
-const form = document.getElementById('main-form')
+const form = document.getElementById('main-form');
+
+//Guardar valores en variable
+const subtotal = document.getElementById('total-bill');
+const porcenDiv = document.getElementById('form_btns') ;
+const personas = document.getElementById('total-people');
+
+//Variable para a;;macenar el valor porcentual seleccionado
+let porceTip;
+
+//evento para escuchar que boton estamos dando click
+porcenDiv.addEventListener('click', (e) => {
+  porcenTip = e.target;
+  console.log(porcenTip);
+
+
+  // condicion para escuchar al custom
+  if(e.target.id === 'custom-tip') {
+    console.log('si soy custom');
+  }
+});
 
 //Crear un evento de submit para la forma
 form.addEventListener('submit', (e) => {
-//Evitar la action por defecto
-e.preventDefault();
+  //Evitar la action por defecto
+  e.preventDefault();
+
+  //crear objeto constante con valores de la forma
+  const formulario ={
+    subtotalF: subtotal.value,
+    porcenTipF: porcenTip.value,
+    personasF: personas.value,
+  }
+  console.log(formulario);
+
+  const { subtotalF, porcenTipF, personasF } = formulario;
+
+  const tipFinal = tipCalculator(subtotalF, porcenTip, personasF);
+
+  console.log(tipFinal);
+
+  const totalF = totalFinal(subtotalF, tipFinal, personasF);
+
+  console.log(totalF);
+
 });
